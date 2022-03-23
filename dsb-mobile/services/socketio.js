9cb31c088ff73fcc6fae34c6f228a3111b91902c
andreocunha <andreoliveiracunha20@gmail.com>
2022-03-23 19:57:27 -0300

    DSB 2022

--- dsb-mobile/services/socketio.js
+++ dsb-mobile/services/socketio.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-// const socket = io.connect('http://192.168.1.157:4000')
-const socket = io.connect('https://server-dsb.herokuapp.com');
+const socket = io.connect('http://192.168.1.157:4000')// mudar para o endereço IP do seu PC
+// const socket = io.connect('https://server-dsb.herokuapp.com');
 
 export default socket;
--- dsb-web/services/socketio.js
+++ dsb-web/services/socketio.js
@@ -1,6 +1,6 @@
 import socketIOClient from 'socket.io-client';
 
-// const socket = socketIOClient("localhost:4000");
-const socket = socketIOClient("server-dsb.herokuapp.com");
+const socket = socketIOClient("localhost:4000");
+// const socket = socketIOClient("server-dsb.herokuapp.com");
 
 export default socket;
